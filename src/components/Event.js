cea70f891b4de643e11c0b665f264d5c73b3c49d
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2024-10-03 18:11:41 +0300

    enhanced structure

--- src/components/Event.js
+++ src/components/Event.js
@@ -9,7 +9,6 @@ const Event = ({event}) => {
         <title>{event.summary}</title>
         <p>{event.location}</p>
         <p>{event.created}</p>
-      </div>
       {showDetails ? (
         <div className = "event-details">
           <p>{event.description}</p>
@@ -20,6 +19,7 @@ const Event = ({event}) => {
       >
         {showDetails ? "Hide Details" : "Show Details"}
       </button>
+      </div>
     </li>
   );
 }
