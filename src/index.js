a9fee46cfba34a4ecd092c05940c3c8c7310995f
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2024-10-14 20:23:52 +0300

    atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('9cbb651b7b4d40d498d752f7336beb61').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
